8d88317c75b65965866b187136877c67835f1526
MarvinPhil <160289274+MarvinPhil@users.noreply.github.com>
2026-01-18 23:43:01 -0500

    added code
    recurssive method added

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
+
+    public static int fibbon(int n) {
+        if (n == 0 || n == 1) {
+            return n;
+        }
+
+        return fibbon(n - 1) + fibbon(n - 2);
+    }
+
     public  static void main(String[] args) {
 
+        int n = 10;
+        int result = fibbon(n);
+        System.out.println("The "+ n +" term in the Fibonacci sequence is "+result);
     }
 }

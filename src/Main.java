f4fd141666f52ad552847f3fc8d755a5bd214cd8
MarvinPhil <160289274+MarvinPhil@users.noreply.github.com>
2026-01-18 23:57:03 -0500

    main.java update
    spaces

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
 
+
     public static int fibbon(int n) {
         if (n == 0 || n == 1) {
             return n;
@@ -8,7 +9,7 @@ public class Main {
         return fibbon(n - 1) + fibbon(n - 2);
     }
 
-    public  static void main(String[] args) {
+    public static void main(String[] args) {
 
         int n = 10;
         int result = fibbon(n);
